e14891fb11635bf44211a9afdc3e11ace970f8eb
Jamesp3301 <a25075@esenviseu.net>
2026-02-18 15:38:08 +0000

    next class van (orginally was gonna be motorcycle but van makes more sense) and vans will cost 15% extra

---
+++ Vehicle-Rental/Van.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Rental
+{
+    public class Van:Vehicle
+    {
+        public Double CargoCap {  get; set; }
+    }
+}
