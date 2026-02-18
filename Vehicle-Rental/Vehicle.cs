2e540483ddc6cc9b31eab0d03b6d3d7063007fe3
Jamesp3301 <a25075@esenviseu.net>
2026-02-18 14:41:45 +0000

    proporties and constructor

--- Vehicle-Rental/Vehicle.cs
+++ Vehicle-Rental/Vehicle.cs
@@ -11,6 +11,20 @@ namespace Vehicle_Rental
         private static int autoId = 0;
         private int id;
         public int Id=>id;
+        public string Brand {  get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal PricePerDay { get; set; }
+        public bool IsRented{ get; set; }
+        public Vehicle(string brand, string model, int year, decimal pricePerDay)
+        {
+            id = ++autoId;  // auto id 
+            Brand = brand;
+            Model = model;
+            Year = year;
+            PricePerDay = pricePerDay;
+            IsRented = false;
+        }
     }
     
 }
