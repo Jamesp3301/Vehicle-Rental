80bd541ff2c3799ef5bf95a1951ddd04d018a044
Jamesp3301 <a25075@esenviseu.net>
2026-02-18 16:03:48 +0000

    Van classe

--- Vehicle-Rental/Van.cs
+++ Vehicle-Rental/Van.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace Vehicle_Rental
 {
-    public class Van:Vehicle
+    public class Van : Vehicle
     {
-        public Double CargoCap {  get; set; }
-    }
+        public Double CargoCap { get; set; }
+        public Van(string brand, string model, int year, decimal priceperday, double cargocap)
+            : base(brand, model, year, priceperday)
+        {
+            CargoCap = cargocap;
+        }
+    
+    public override decimal CalculateRentalCost(int days)
+        {
+            return (PricePerDay * days) * 1.15m;
+        }
+        public override string GetVehicleType()
+        {
+            return "Van";
+        }
+    }   
 }
